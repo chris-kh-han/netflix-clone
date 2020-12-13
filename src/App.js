7c6c9c1e28cfa4ff40b8bef6a01647ce72c7d602
Chris KH Han <chris.kh.han@gmail.com>
2020-12-13 07:57:05 -0600

    update path for each route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import * as ROUTES from './constants/routes'
 import Home from './pages/home'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path={ROUTES.HOME}>
           <Home />
         </Route>
-        <Route exact path="/signup">
+        <Route path={ROUTES.SIGN_UP}>
           <p>I will be the sign un page</p>
         </Route>
-        <Route exact path="/signin">
+        <Route path={ROUTES.SIGN_IN}>
           <p>I will be the sign in page</p>
         </Route>
-        <Route exact path="/browse">
+        <Route path={ROUTES.BROWSE}>
           <p>I will be the browse page</p>
         </Route>
       </Switch>
