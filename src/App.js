a4cdc512a4f318559cca34fd1e3f35520d731111
Chris KH Han <chris.kh.han@gmail.com>
2020-12-13 02:10:14 -0600

    create Jumbotron Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from './home/Home'
+import Home from './pages/home'
+
 function App() {
   return (
     <Router>
---
+++ src/containers/jumbotron.js
@@ -0,0 +1,24 @@
+import React from "react";
+import jumboDate from "../fixtures/jumbo.json";
+import Jumbotron from "../components/jumbotron";
+
+export function JumbotronContainer() {
+  return (
+    <Jumbotron.Container>
+      {jumboDate.map((jumbo) => (
+        <Jumbotron key={jumbo.id} direction={jumbo.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{jumbo.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{jumbo.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image
+              src={jumbo.image}
+              alt={jumbo.alt}
+            ></Jumbotron.Image>
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
+}
