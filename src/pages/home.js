6896c0ad34ffe15da216a7d72b58eb7d096c8db6
Chris KH Han <chris.kh.han@gmail.com>
2020-12-13 08:29:51 -0600

    add Feature Component to home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import { JumbotronContainer } from '../containers/jumbotron'
-import { FooterContainer } from '../containers/footer'
-import { FaqsContainer } from '../containers/faqs'
-import { HeaderContainer } from '../containers/header'
+import React from "react";
+import { OptForm, Feature } from "../components";
+import { JumbotronContainer } from "../containers/jumbotron";
+import { FooterContainer } from "../containers/footer";
+import { FaqsContainer } from "../containers/faqs";
+import { HeaderContainer } from "../containers/header";
 
 export default function Home() {
-    return (
-        <>  
-            <HeaderContainer />
-            <JumbotronContainer />
-            <FaqsContainer />
-            <FooterContainer />
-        </>
-    )
+  return (
+    <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited Films, TV Programs and More.</Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try It Now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to Watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
+
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
 }
