c762ff357dfb65a23ccb0b95d3fa7cb7d9588916
Chris KH Han <chris.kh.han@gmail.com>
2020-12-14 06:23:11 -0600

    update view pages

---
+++ src/pages/browse.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { BrowseContainer } from '../containers/browse'
+
+export default function Browse() {
+    return (
+        <BrowseContainer>
+        </BrowseContainer>
+    )
+}
