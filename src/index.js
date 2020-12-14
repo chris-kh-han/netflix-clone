e9adf3936ef8ad4328145a35fd0af09cb6f22c2d
Chris KH Han <chris.kh.han@gmail.com>
2020-12-14 06:21:03 -0600

    add firebase Config on index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { GlobalStyles } from './global-styles';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { GlobalStyles } from "./global-styles";
+import App from "./App";
+import { FirebaseContext } from "./context/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8wDeEQnrIrUMlCem3h1eO98x6EEupKTw",
+  authDomain: "netflix-clone-d6714.firebaseapp.com",
+  databaseURL: "https://netflix-clone-d6714.firebaseio.com",
+  projectId: "netflix-clone-d6714",
+  storageBucket: "netflix-clone-d6714.appspot.com",
+  messagingSenderId: "949677891578",
+  appId: "1:949677891578:web:db80d7160ba672f59a4e43",
+  measurementId: "G-5NXZJ3XVGR"
+};
+
+const firebase = window.firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+
 
