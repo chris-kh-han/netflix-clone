5864afe19da16d3393ad11e21e560943cf7bde5f
Chris KH Han <chris.kh.han@gmail.com>
2020-12-18 11:00:49 -0600

    create Header Component

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -6,7 +6,7 @@ export const Background = styled.section`
     flex-direction: column;
     background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')}) top left / cover no-repeat;
     
-    @media (max-width: 1100px) {
+    @media (max-width: 900px) {
         ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
     }
 `
@@ -22,7 +22,7 @@ export const Container = styled.div`
         display: flex;
     }
     
-    @media (max-width: 1000px) {
+    @media (max-width: 1100px) {
         margin: 0 30px;
     }
 `;
@@ -72,148 +72,148 @@ export const Logo = styled.img`
     }
 `;
 
-// export const Group = styled.div`
-//     display: flex;
-//     align-items: center;
-// `;
+export const Group = styled.div`
+    display: flex;
+    align-items: center;
+`;
 
-// export const Text = styled.p`
-//     color: white;
-//     font-size: 22px;
-//     line-height: normal;
-//     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
-// `;
+export const Text = styled.p`
+    color: white;
+    font-size: 22px;
+    line-height: normal;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+`;
 
-// export const Feature = styled(Container)`
-//     padding: 150px 0 500px 0;
-//     flex-direction: column;
-//     align-items: normal;
-//     width: 50%;
+export const Feature = styled(Container)`
+    padding: 150px 0 500px 0;
+    flex-direction: column;
+    align-items: normal;
+    width: 50%;
     
-//     @media (max-width: 1100px) {
-//         display: none;
-//     }
-// `;
+    @media (max-width: 900px) {
+        display: none;
+    }
+`;
 
-// export const FeatureCallOut = styled.h2`
-//     color: white;
-//     font-size: 50px;
-//     line-height: normal;
-//     font-weight: bold;
-//     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
-//     margin: 0;
-// `;
+export const FeatureCallOut = styled.h2`
+    color: white;
+    font-size: 50px;
+    line-height: normal;
+    font-weight: bold;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+    margin: 0;
+`;
 
-// export const PlayButton = styled.button`
-//     box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
-//     background-color: #e6e6e6;
-//     color: #000;
-//     border-width: 0;
-//     padding: 10px 20px;
-//     border-radius: 5px;
-//     max-width: 130px;
-//     font-weight: bold;
-//     font-size: 20px;
-//     margin-top: 10px;
-//     cursor: pointer;
-//     transition: background-color: 0.5s ease;
+export const PlayButton = styled.button`
+    box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
+    background-color: #e6e6e6;
+    color: #000;
+    border-width: 0;
+    padding: 10px 20px;
+    border-radius: 5px;
+    max-width: 130px;
+    font-weight: bold;
+    font-size: 20px;
+    margin-top: 10px;
+    cursor: pointer;
+    transition: background-color: 0.5s ease;
     
-//     &:hover {
-//         background: #ff1e1e;
-//         color: white;
-//     }
-// `;
+    &:hover {
+        background: #ff1e1e;
+        color: white;
+    }
+`;
 
-// export const Search = styled.div`
-//     display: flex;
-//     align-items: center;
+export const Search = styled.div`
+    display: flex;
+    align-items: center;
     
-//     @media (max-width: 700px) {
-//         display: none;
-//     }
-// `;
+    @media (max-width: 700px) {
+        display: none;
+    }
+`;
 
-// export const SearchIcon = styled.button`
-//     cursor: pointer;
-//     background-color: transparent;
-//     border: 0;
+export const SearchIcon = styled.button`
+    cursor: pointer;
+    background-color: transparent;
+    border: 0;
     
-//     img {
-//         filter: brightness(0) invert(1);
-//         width: 16px;
-//     }
-// `
+    img {
+        filter: brightness(0) invert(1);
+        width: 16px;
+    }
+`
 
-// export const SearchInput = styled.input`
-//     background-color: #44444459;
-//     color: white;
-//     border: 1px solid white;
-//     transition: width 0.5s;
-//     height: 30px;
-//     font-size: 14px;
-//     margin-left: ${({ active }) => (active === true ? '10px' : '0')};
-//     padding: ${({ active }) => (active === true ? '0 10px' : '0')};
-//     opacity: ${({ active }) => (active === true ? '1' : '0')};
-//     width: ${({ active }) => (active === true ? '200px' : '0px')};
-// `;
+export const SearchInput = styled.input`
+    background-color: #44444459;
+    color: white;
+    border: 1px solid white;
+    transition: width 0.5s;
+    height: 30px;
+    font-size: 14px;
+    margin-left: ${({ active }) => (active === true ? '10px' : '0')};
+    padding: ${({ active }) => (active === true ? '0 10px' : '0')};
+    opacity: ${({ active }) => (active === true ? '1' : '0')};
+    width: ${({ active }) => (active === true ? '200px' : '0px')};
+`;
 
-// export const Picture = styled.button`
-//     background: url(${({ src }) => src});
-//     background-size: contain;
-//     border: 0;
-//     width: 32px;
-//     height: 32px;
-//     cursor: pointer;
-// `;
+export const Picture = styled.button`
+    background: url(${({ src }) => src});
+    background-size: contain;
+    border: 0;
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+`;
 
-// export const Dropdown = styled.div`
-//     display: none;
-//     position: absolute;
-//     background-color: black;
-//     padding: 10px;
-//     width: 100px;
-//     top: 32px;
-//     right: 10px;
+export const Dropdown = styled.div`
+    display: none;
+    position: absolute;
+    background-color: black;
+    padding: 10px;
+    width: 100px;
+    top: 32px;
+    right: 10px;
     
-//     ${Group}:last-of-type ${Link} {
-//         cursor: pointer;
-//     }
+    ${Group}:last-of-type ${Link} {
+        cursor: pointer;
+    }
     
-//     ${Group} {
-//         margin-bottom: 10px;
+    ${Group} {
+        margin-bottom: 10px;
         
-//         &:last-of-type {
-//             margin-bottom: 0;
-//         }
+        &:last-of-type {
+            margin-bottom: 0;
+        }
         
-//         ${Link}, ${Picture} {
-//             cursor: default;
-//         }
-//     }
+        ${Link}, ${Picture} {
+            cursor: default;
+        }
+    }
     
-//     button {
-//         margin-right: 10px;
-//     }
+    button {
+        margin-right: 10px;
+    }
     
-//     p {
-//         font-size: 12px;
-//         margin-bottom: 0;
-//         margin-top: 0;
-//     }
-// `;
+    p {
+        font-size: 12px;
+        margin-bottom: 0;
+        margin-top: 0;
+    }
+`;
 
-// export const Profile = styled.div`
-//     display: flex;
-//     align-items: center;
-//     margin-left: 20px;
-//     position: relative;
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+    position: relative;
     
-//     button {
-//         cursor: pointer;
-//     }
+    button {
+        cursor: pointer;
+    }
     
-//     &:hover > ${Dropdown} {
-//         display: flex;
-//         flex-direction: column;
-//     }
-// `;
+    &:hover > ${Dropdown} {
+        display: flex;
+        flex-direction: column;
+    }
+`;
