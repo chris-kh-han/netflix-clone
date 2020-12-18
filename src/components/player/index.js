bf4f2da19cec566575a6bb42badc41c0cb4aee54
Chris KH Han <chris.kh.han@gmail.com>
2020-12-18 11:01:16 -0600

    create Player Component

---
+++ src/components/player/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext, createContext } from "react";
+import ReactDom from "react-dom";
+import { Container, Button, Overlay, Inner, Close } from './styles/player';
+
+export const PlayerContext = createContext();
+
+export default function Player({ children, ...restProps }) {
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  return (
+    <PlayerContext.Provider value={{ showPlayer, setShowPlayer }}>
+      <Container {...restProps}>{children}</Container>
+    </PlayerContext.Provider>
+  );
+}
+
+Player.Video = function PlayerVideo({ ...restProps }) {
+  const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+
+  return showPlayer
+    ? ReactDom.createPortal(
+        <Overlay onClick={() => setShowPlayer(false)}>
+          <Inner>
+            <video id="netflix-player" controls>
+              <source src="/videos/bunny.mp4" />
+            </video>
+            <Close />
+          </Inner>
+          
+        </Overlay>,
+        document.body
+      )
+    : null;
+};
+
+Player.Button = function PlayerButton({ ...restProps }) {
+    const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+    
+    return <Button onClick={() => setShowPlayer(!showPlayer)}>Play</Button>;
+};
